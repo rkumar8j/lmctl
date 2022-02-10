8fa92b028086691de88174ad3c74d4b519c1fef4
Manoj Natarajan <manojn97@in.ibm.com>
2022-02-10 12:18:48 +0530

    Update brent_src.py

--- lmctl/project/handlers/brent/brent_src.py
+++ lmctl/project/handlers/brent/brent_src.py
@@ -474,15 +474,19 @@ class BrentSourceCreatorDelegate(handlers_api.ResourceSourceCreatorDelegate):
             descriptor.add_property('flavourId', description='Identifier of the VNF DF to be instantiated', ptype='string', required=True)
             descriptor.add_property('instantiationLevelId', description='Identifier of the instantiation level of the deployment flavour to be instantiated. If not present, the default instantiation level as declared in the VNFD is instantiated', \
                 ptype='string')
-            descriptor.add_property('localizationLanguage', description='Localization language of the VNF to be instantiated', ptype='string')
-            descriptor.insert_lifecycle('Create')
+            descriptor.add_property('localizationLanguage', description='Localization language of the VNF to be instantiated', ptype='string')    
             descriptor.insert_lifecycle('Install')
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
             descriptor.insert_lifecycle('Configure')
 >>>>>>> parent of c69117b (Update brent_src.py)
+=======
+            descriptor.insert_lifecycle('Configure')
+            descriptor.insert_lifecycle('Start')
+            descriptor.insert_lifecycle('Stop')
+>>>>>>> parent of b6aa9ce (code fix)
             descriptor.insert_lifecycle('Uninstall')
-            descriptor.insert_lifecycle('Delete')
         elif lifecycle_type == LIFECYCLE_TYPE_SOL005:
             file_ops.append(handlers_api.CreateDirectoryOp(source_tree.sol005_lifecycle_path, handlers_api.EXISTING_IGNORE))
             sol005_tree = Sol005LifecycleTree(source_tree.sol005_lifecycle_path)
